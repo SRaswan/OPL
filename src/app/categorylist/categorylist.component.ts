b7110361b4f7e5a392b8b3917a1bad18cac17ec0
LegoAvatar04 <shaurya@gmail.com>
2020-11-02 20:08:43 -0800

    fixed icons on category list

--- src/app/categorylist/categorylist.component.ts
+++ src/app/categorylist/categorylist.component.ts
@@ -17,8 +17,8 @@ export class CategorylistComponent implements OnInit {
    sub_category_id: string;
    category_id: string;
 
-   cardcolors: string[] = ["primary", "info", "warning", "success", "secondary", "light", "dark"];
-   cardicons: string[] = ["fa-laptop", "fa-calculator", "fa-music", "fa-flask","fa-cogs"];
+   cardcolors: string[] = ["primary", "secondary", "info", "warning", "success", "secondary", "light", "dark"];
+   cardicons: string[] = ["fa-laptop", "fa-globe-americas", "fa-calculator", "fa-music", "fa-flask","fa-cogs"];
 
    private sub: any;
    private sub_cat: any;
